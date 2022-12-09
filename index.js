3d5188dd26a446efd29d0967938f8e0979248845
Alvin Mithamo <117805605+alvinsleek@users.noreply.github.com>
2022-12-09 13:11:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
 // Write your code here
-var multiply = num1 * num2;
+//var multiply = num1 * num2;
+const num1 = 31;
+const num2 = 2;
 
-var random = Math.ceil(Math.random() * 10);
+const multiply = 31 * 2;
 
-var mod = num3 % num4;
 
-var max = Math.max(10, 20);
+//var random = Math.ceil(Math.random() * 10);
+const random = Math.ceil(Math.random());
+
+//var mod = num3 % num4;
+const num3 = 20;
+const num4 = 8;
+const mod = num3%num4;
+
+//var max = Math.max(10, 20);
+const max = Math.max(8,12,1,20);
